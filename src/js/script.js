449a23123bd0d42829646e3f61f5dceee4b1aa69
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-06-14 23:15:47 -0300

    Add console.log c/ template string

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,4 @@ const sobrenome = "Pereira";
 const anoNascimento = "1994";
 const cidade = "Fortaleza";
 let idade = 2022 - anoNascimento;
-
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}.`);
