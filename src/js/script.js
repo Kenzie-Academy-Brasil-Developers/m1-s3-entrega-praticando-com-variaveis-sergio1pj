b3b316c5b595910b95c9bba153ad3c8aa191d5a0
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-06-14 23:17:23 -0300

    Alterando de const pra let

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-const nome = "Sergio";
-const sobrenome = "Pereira";
-const anoNascimento = "1994";
-const cidade = "Fortaleza";
+let nome = "Sergio";
+let sobrenome = "Pereira";
+let anoNascimento = "1994";
+let cidade = "Fortaleza";
 let idade = 2022 - anoNascimento;
 console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}.`);
